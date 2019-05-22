eed65bed72aaa6b83d0ce37e59e4c048bc7993c9
zxn721 <zhang721688@163.com>
2019-05-22 15:10:50 +0800

    升级1.1.9

--- PresenterLib/src/main/java/com/zxn/presenter/model/CommonEventBus.java
+++ PresenterLib/src/main/java/com/zxn/presenter/model/CommonEventBus.java
@@ -9,6 +9,24 @@ import org.greenrobot.eventbus.EventBus;
 public class CommonEventBus {
 
     /**
+     * 注册订阅
+     *
+     * @param subscriber 订阅者.
+     */
+    public void register(Object subscriber) {
+        EventBus.getDefault().register(subscriber);
+    }
+
+    /**
+     * 反注册订阅
+     *
+     * @param subscriber 订阅者.
+     */
+    public void unregister(Object subscriber) {
+        EventBus.getDefault().unregister(subscriber);
+    }
+
+    /**
      * EventBus发送消息.
      *
      * @param event 通用的CommonEvent
@@ -18,12 +36,9 @@ public class CommonEventBus {
     }
 
     /**
-     *
-     *
-     */
-    /**
      * Posts the given event to the event bus and holds on to the event (because it is sticky).
      * The most recent sticky event of an event's type is kept in memory for future access by subscribers using Subscribe#sticky.
+     *
      * @param event 通用的CommonEvent
      */
     public static void postSticky(CommonEvent event) {
